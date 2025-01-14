5521076dfcfd81cfd962ae80c1c47328f1e0af6f
ApoKads <jonathantjahjana@gmail.com>
2025-01-14 18:04:58 +0700

    first push

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Judul</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#F8F7F2",
+        ourLime: "#CED629",
+        ourBlue: "#5567AF",
+        ourPink: "#EB1763",
+        ourOrange:"#F27825",
+        fontBlueHome: "#4B578F",
+        fontGrayHome: "#7B8B99"
+      },
+      fontFamily:{
+        'pop':['Poppins','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
